275f13fa060c446b09048cf4d2fd9905558ddce2
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-12-28 08:48:28 +0200

    Initial config

---
+++ lua/custom/plugins/overrides.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+     -- "tsserver",
+    "c",
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+
+    -- rust stuff
+    "rust-analyzer",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
